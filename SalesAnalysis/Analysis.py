71caadd8ad214ea73cb436628a351ff2f03a5112
Felix <felix.wolter00@gmail.com>
2023-11-02 10:01:59 +0100

    Data Cleaning and Best Month

--- SalesAnalysis/Analysis.py
+++ SalesAnalysis/Analysis.py
@@ -1,4 +1,20 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
+# Read csv
+all_sales = pd.read_csv('all_data.csv')
 
+# Data cleaning
+all_sales = all_sales.dropna()
+all_sales = all_sales[all_sales['Order Date'].str[0:2]!='Or']
+all_sales['Quantity Ordered'] = pd.to_numeric(all_sales['Quantity Ordered'])
+all_sales['Price Each'] = pd.to_numeric(all_sales['Price Each'])
 
+# Best Month
+all_sales['month'] = all_sales['Order Date'].str.split('/').str[0]
+all_sales['price'] = all_sales['Quantity Ordered'] * all_sales['Price Each']
+all_sales_by_month = all_sales.groupby('month')['price'].sum()
+
+# show bar graph
+# plt.bar(range(1,13), all_sales_by_month)
+# plt.show()
